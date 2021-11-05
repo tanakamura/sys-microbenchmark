2f62b901c202390835e2ce0423ffd035dd807ffe
tanakmura <tanakmura@gmail.com>
2021-11-06 03:04:01 +0900

    pages

--- gen-page.sh
+++ gen-page.sh
@@ -4,3 +4,4 @@ cp build-em/sys-microbenchmark.js docs/viewer
 cp build-em/sys-microbenchmark.wasm docs/viewer
 cp build-em/view.html docs/viewer
 cp build-em/view.js docs/viewer
+cp build-em/combined.json docs/viewer
